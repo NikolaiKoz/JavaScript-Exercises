4aacbb949533e1861fe487bf6fbae4715d701df8
Vallejos Jose Luis <joseluisvallejos1995@gmail.com>
2022-10-14 19:21:45 -0300

    :pencil: loiasfhasolifholaisfhoiahf.

--- 12-10-2022/index.js
+++ 12-10-2022/index.js
@@ -1,1 +1,47 @@
-console.log("Hola Mundo!");
+var name = "Jose Luis Vallejos";
+console.log(name);
+
+var age = (2022 - 1995);
+console.log(age);
+
+var married = false;
+console.log(married);
+
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(numbers);
+
+var friends = ["Juan", "Ivan", "Cesar", "Leonardo", "Fabian"];
+console.log(friends);
+
+var object = {
+
+    nameObject: "Jose Luis Vallejos",
+    ageObject: (2022 - 1995),
+    marriedObject: false,
+    numbersObject: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    friendsObject: ["Juan", "Ivan", "Cesar", "Leonardo", "Fabian"],
+
+};
+console.log(object);
+
+var User = {
+    favoriteNumbers: [1, 2, 3, 4, 5],
+    typemascotas: "Dog",
+    mascotas : {
+
+        pet1 : {
+            name: "Marmota",
+            age: 10 + " years old",
+            race: "Labrador",
+        },
+
+        pet2 : {
+            name : "Perla",
+            age: 5 + "years old",
+            rece: "Unknown",
+        },
+    },
+    favoriteBand : "Linkin Park"
+}
+
+console.log(User);
