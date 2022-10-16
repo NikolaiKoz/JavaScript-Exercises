2b35f2b89b271a85d4ed4b581febecc9d888e4e8
Vallejos Jose Luis <joseluisvallejos1995@gmail.com>
2022-10-15 23:28:07 -0300

    :pencil: Exercices.

--- 12-10-2022/index.js
+++ 12-10-2022/index.js
@@ -45,3 +45,24 @@ var User = {
 }
 
 console.log(User);
+
+
+//Como funciona las expresiones ternarias????
+
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+  return strokes === 1
+    ? names[0]
+    : strokes <= par - 2
+    ? names[1]
+    : strokes === par - 1
+    ? names[2]
+    : strokes === par
+    ? names[3]
+    : strokes === par + 1
+    ? names[4]
+    : strokes === par + 2
+    ? names[5]
+    : names[6];
+}
