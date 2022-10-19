79503e797353a16d2b426ec8a9dfe97cffb04fee
Vallejos Jose Luis <joseluisvallejos1995@gmail.com>
2022-10-18 23:56:49 -0300

    :package: Installing the babel package and configuration.

---
+++ ECAMSCRIPT 6/PROYECT 1/output/const_let.js
@@ -0,0 +1,7 @@
+"use strict";
+
+console.log("Hola mundo");
+var nombre = "Jose Luis";
+var lastName = "Jose Luis";
+var name = "Jose Luis";
+console.log("var ".concat(nombre, ", let ").concat(lastName, ", const ").concat(name));
