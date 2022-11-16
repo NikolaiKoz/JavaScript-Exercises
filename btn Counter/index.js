10ba1aae0940f12fa57ebfb7db6d5a95bd7dc3be
Vallejos Jose Luis <joseluisvallejos1995@gmail.com>
2022-11-16 19:03:37 -0300

    :bookmark: Counter exercite done.

---
+++ btn Counter/index.js
@@ -0,0 +1,44 @@
+/*
+El contador debe iniciar en 0. ---
+Debe tener un botón para agregar una unidad (+) y quitar una unidad (-).
+No debe permitir número negativos. Se puede utilizar la property "Disabled"
+Debe tener un límite de 50. Se puede utilizar la property "Disabled"
+Al alcanzar cualquiera de los límites debe generar una interacción con el user. (Mensaje en pantalla que arranque en display:none y cambie de display, pueden utilizar un alert, etc)
+
+*/
+const add = document.getElementById("add");
+const subtract = document.getElementById("subtract");
+const display = document.getElementById("display");
+
+
+let counter = 0;
+
+add.addEventListener("click", () => {
+    if( counter < 5 ){
+        counter++;
+        display.innerHTML = `Counter N° ${counter}`;
+    }
+    subtract.disabled = false;
+    if( counter === 5){
+        alert("MAX NUMBER");
+        add.disabled = true;
+    }
+
+});
+
+subtract.addEventListener("click", () => {
+    if( counter > 0 ){
+        counter--;
+        display.innerHTML = `Counter N° ${counter}`;
+        add.disabled = false;
+    }
+
+    if( counter === 0){
+
+        alert("MIN NUMBER");
+        subtract.disabled = true;
+    }
+});
+
+
+
